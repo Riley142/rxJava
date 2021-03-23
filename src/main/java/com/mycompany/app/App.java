0ddad81ea61e406f5d0764b847fa7cea9019b02c
Riley <rileyxcroft@gmail.com>
2021-03-23 10:08:06 +0000

    Added few comments

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,24 @@
 package com.mycompany;
 
+import com.mycompany.app.Whatever;
 import io.reactivex.rxjava3.core.*;
 
 public class App {
     public static void main(String[] args) {
-        Flowable.just("Hello world").subscribe(System.out::println);
+
+        Whatever whatever = new Whatever();
+        Observable<String> obsv = whatever.gamesForSale();
+
+        //subscribing to Observalbing
+        obsv.subscribe(
+                // we got 3 lambdas
+
+                //data for sending
+                data -> System.out.println("We got some data: " + data),
+                //error handling
+                error -> System.out.println("Error occured..." + error),
+                //for handling completition
+                () -> System.out.println("We are done... Thank You for the data...")
+        );
     }
 }
--- src/main/java/com/mycompany/app/Whatever.java
+++ src/main/java/com/mycompany/app/Whatever.java
@@ -1,2 +1,42 @@
-package com.mycompany.app;public class Whatever {
+package com.mycompany.app;
+import io.reactivex.rxjava3.core.Observable;
+import java.util.Arrays;
+import java.util.List;
+
+public class Whatever {
+
+    public Observable<String> gamesForSale() {
+        List<String> games = Arrays.asList(
+                "game1",
+                "game2",
+                "game3"
+        );
+        return Observable.create(emmiter -> {
+            //set index to 0;
+            int i = 0;
+            System.out.println("Start sending games");
+            //while loop
+
+            // ask if emitter is on disposed meaning:
+            // subscriber could bue unsubscribed
+            // in that case inDisposed would be true
+            // then we shouldn't send data anyway
+
+            //if the index is lower than game.size
+            // to make sure we don't get index out of bound exepction
+            while (!emmiter.isDisposed() && i < games.size()) {
+              //  here we get our game
+                String game = games.get(i);
+                //onNext sends data one by one
+                emmiter.onNext(game);
+                //increment index with each run of the loop
+                i++;
+            }
+            System.out.println("Done game sending");
+            //tells  that no more next calls will occur, emission complete
+            emmiter.onComplete();
+        });
+    }
 }
+
+//noticed in this program thrre is no onError
